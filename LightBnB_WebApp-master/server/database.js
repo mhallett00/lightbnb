238c455f579241bd0afeef9ac3e655e68380d9c7
Michael Hallett <m.hallett@gmail.com>
2020-05-28 20:54:11 +0000

    Updated reservation query to show all reservation instead of only future reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,7 +80,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id
   WHERE reservations.guest_id = $1
-  AND reservations.end_date > Now()::date
   GROUP BY properties.id, reservations.id
   ORDER BY reservations.start_date
   LIMIT $2;
