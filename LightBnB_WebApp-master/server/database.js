f8424d85acb0fb635f45a4800b06d439935fb32a
Michael Hallett <m.hallett@gmail.com>
2020-05-28 20:36:41 +0000

    Changed My reservations to query database instead of JSON

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,7 +74,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.email
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date > Now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `,[guest_id, limit])
+  .then (res => {
+    console.log(res.rows)
+    return res.rows;
+  });
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
