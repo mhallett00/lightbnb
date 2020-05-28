811faa30c25ebe62e16a71a98db47611640328b0
Michael Hallett <m.hallett@gmail.com>
2020-05-28 01:35:37 +0000

    Removed delete clauses

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,8 +1,3 @@
-DELETE FROM users;
-DELETE FROM properties;
-DELETE FROM reservations;
-DELETE FROM property_values;
-
 INSERT INTO users 
 VALUES (1, 'Jimmy John', 'jj@fakemail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 (2, 'Tom Bollard', 'tb@fakemail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'), (3, 'Dan Packer', 'dp@fakemail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
@@ -10,7 +5,6 @@ VALUES (1, 'Jimmy John', 'jj@fakemail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/
 INSERT INTO properties 
 VALUES (1, 1, 'Colourful Building', 'description', 'https://www.pexels.com/photo/contemporary-design-112811/', 'https://www.pexels.com/photo/apartment-balcony-buildings-city-439391/', 350, 1, 1, 1, 'Canada', '123 EZ street', 'Vancourver', 'BC', '789 123', true ),
 (2, 1, 'In The Sky', 'description', 'https://www.pexels.com/photo/photo-of-living-room-1457842/', 'https://www.pexels.com/photo/apartment-apartment-building-architecture-building-323705/', 400, 0, 1, 2, 'Canada', '456 lost lane', 'Edmonton', 'AL', '456 789', true),
- 
 (3, 2,'Whitespace', 'description', 'https://www.pexels.com/photo/apartment-architecture-bookcase-bookshelves-271816/', 'https://www.pexels.com/photo/apartment-architecture-balcony-building-259950/', 450, 2, 2, 1, 'Canada', '789 clairvoyance', 'Toronto', 'ON', '123 456', true);
 
 INSERT INTO reservations
