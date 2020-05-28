08a88f437ac68ce9443ce7b6e3b53a5eddb8752b
Michael Hallett <m.hallett@gmail.com>
2020-05-28 01:35:13 +0000

    Reordered reservation keys

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -32,10 +32,10 @@ CREATE TABLE properties (
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
 );
 
 CREATE TABLE property_reviews (
